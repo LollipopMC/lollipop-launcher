3788c89663371ef0283f3e71a16075087f137031
zeithrold <zeithrold@gmail.com>
2024-07-09 09:45:12 +0800

    debug: limit debug mode only activate on NODE_ENV==='development'

--- src/components/debug/index.tsx
+++ src/components/debug/index.tsx
@@ -3,6 +3,9 @@ import Error from './Error'
 import AlertBar from './AlertBar'
 
 export default function Debug() {
+  if (import.meta.env.PROD) {
+    return null
+  }
   return (
     <>
       <Error />
